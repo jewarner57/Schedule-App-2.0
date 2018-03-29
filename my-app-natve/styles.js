cc5d34e3c5284fea7065beabb4f21401eee5bfa9
Jonathan Warner <jonathanwarner@Jonathans-MacBook-Pro.local>
2018-03-29 15:30:25 -0400

    03/29/18  - Finished Adding delete functionality

--- my-app-natve/styles.js
+++ my-app-natve/styles.js
@@ -1,18 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import Note from './Note.js';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        
-        <Note></Note>
-        
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
     
